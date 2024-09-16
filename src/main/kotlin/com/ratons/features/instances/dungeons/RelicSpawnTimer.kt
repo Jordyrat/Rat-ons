ac5b9575995de4e358a8bc6b1c8aa03bf82c71dd
ItsEmpa <itsempa@users.noreply.github.com>
2024-09-16 19:45:42 +0200

    ballers

--- src/main/kotlin/com/ratons/features/instances/dungeons/RelicSpawnTimer.kt
+++ src/main/kotlin/com/ratons/features/instances/dungeons/RelicSpawnTimer.kt
@@ -1,10 +1,17 @@
 package com.ratons.features.instances.dungeons
 
-import at.hannibal2.skyhanni.events.DungeonStartEvent
 import at.hannibal2.skyhanni.events.LorenzChatEvent
+import at.hannibal2.skyhanni.events.LorenzRenderWorldEvent
+import at.hannibal2.skyhanni.events.LorenzTickEvent
+import at.hannibal2.skyhanni.events.LorenzWorldChangeEvent
 import at.hannibal2.skyhanni.features.dungeon.DungeonAPI
-import at.hannibal2.skyhanni.utils.RegexUtils.matches
+import at.hannibal2.skyhanni.utils.LocationUtils
+import at.hannibal2.skyhanni.utils.LocationUtils.distanceSqToPlayer
+import at.hannibal2.skyhanni.utils.LorenzColor
+import at.hannibal2.skyhanni.utils.LorenzVec
+import at.hannibal2.skyhanni.utils.RenderUtils.drawString
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
+import at.hannibal2.skyhanni.utils.TimeUtils.format
 import com.ratons.Ratons
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import kotlin.time.Duration.Companion.seconds
@@ -14,31 +21,58 @@ object RelicSpawnTimer {
 
     private val config get() = Ratons.feature.instancesConfig.dungeonsCategory.relicSpawnTimer
 
-    private val startPattern = "§r§4\\[BOSS] Necron§r§c: §r§cARGH!§r".toPattern()
+    private const val START_MESSAGE = "§4[BOSS] Necron§r§c: §r§cARGH!"
 
     private var messagesSentAmount = 0
+    private var timerEnd = SimpleTimeMark.farPast()
+
+    private val locations = listOf(
+        LorenzVec(56, 8, 132),
+        LorenzVec(20, 6, 94),
+        LorenzVec(20, 6, 59),
+        LorenzVec(91, 6, 94),
+        LorenzVec(92, 6, 56),
+    )
+
+    private var location: LorenzVec? = null
 
     @SubscribeEvent
-    fun onDungeonStart(event: DungeonStartEvent) {
+    fun onDungeonStart(event: LorenzWorldChangeEvent) {
         messagesSentAmount = 0
+        location = null
     }
 
+    @SubscribeEvent
     fun onChat(event: LorenzChatEvent) {
-        if (!DungeonAPI.inDungeon()) return
-        if (startPattern.matches(event.message)) {
-            relicTimer()
+        if (!isEnabled()) return
+        if (START_MESSAGE == event.message) {
+            startTimer()
         }
     }
 
-    private fun relicTimer() {
+    private fun startTimer() {
         ++messagesSentAmount
         if (messagesSentAmount != 2) return
-        val timerEnd = (SimpleTimeMark.now() + 4.seconds)
-        var timerDisplay = timerEnd.timeUntil()
-        while (timerDisplay > 0.seconds && DungeonAPI.inDungeon()) {
-            var timerDisplay = timerEnd.timeUntil()
-            // TODO: Draw timer hologram above/below relic
-        }
+        timerEnd = SimpleTimeMark.now() + 4.seconds
     }
+
+    @SubscribeEvent
+    fun onTick(event: LorenzTickEvent) {
+        if (!isEnabled()) return
+        location = locations.minBy { it.distanceSqToPlayer() }
+    }
+
+    @SubscribeEvent
+    fun onRenderWorld(event: LorenzRenderWorldEvent) {
+        if (!isEnabled()) return
+        if (timerEnd.isInPast()) return
+        val pos = location ?: return
+        if (LocationUtils.playerLocation().y > 20) return
+        val text = timerEnd.timeUntil().format(showMilliSeconds = true)
+        event.drawString(pos, text, true, LorenzColor.LIGHT_PURPLE.toColor())
+    }
+
+    private fun isEnabled() =
+        DungeonAPI.inDungeon() && DungeonAPI.isOneOf("M7") && DungeonAPI.inBossRoom && config
 
 }
