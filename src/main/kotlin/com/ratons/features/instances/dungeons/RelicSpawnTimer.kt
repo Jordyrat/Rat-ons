eabed94e1c334236e9fd41f6a92724ad527d7214
ItsEmpa <itsempa@users.noreply.github.com>
2024-09-25 16:18:03 +0200

    only render specific class relic

--- src/main/kotlin/com/ratons/features/instances/dungeons/RelicSpawnTimer.kt
+++ src/main/kotlin/com/ratons/features/instances/dungeons/RelicSpawnTimer.kt
@@ -4,6 +4,7 @@ import at.hannibal2.skyhanni.events.LorenzChatEvent
 import at.hannibal2.skyhanni.events.LorenzRenderWorldEvent
 import at.hannibal2.skyhanni.events.LorenzWorldChangeEvent
 import at.hannibal2.skyhanni.features.dungeon.DungeonAPI
+import at.hannibal2.skyhanni.utils.LorenzColor
 import at.hannibal2.skyhanni.utils.LorenzVec
 import at.hannibal2.skyhanni.utils.RenderUtils.drawString
 import at.hannibal2.skyhanni.utils.SimpleTimeMark
@@ -22,23 +23,13 @@ object RelicSpawnTimer {
 
     private var messagesSentAmount = 0
     private var timerEnd = SimpleTimeMark.farPast()
-
-    private val locations = listOf(
-        LorenzVec(56, 8, 132),
-        LorenzVec(20, 6, 94),
-        LorenzVec(20, 6, 59),
-        LorenzVec(91, 6, 94),
-        LorenzVec(92, 6, 56),
-    )
-
-    /*private val location by RecalculatingValue(1.ticks) {
-        locations.minBy { it.distanceToPlayerSqIgnoreY() }
-    }*/
+    private var relic: Relic? = null
 
     @SubscribeEvent
     fun onWorldChange(event: LorenzWorldChangeEvent) {
         messagesSentAmount = 0
         timerEnd = SimpleTimeMark.farPast()
+        relic = null
     }
 
     @SubscribeEvent
@@ -51,23 +42,52 @@ object RelicSpawnTimer {
 
     private fun startTimer() {
         ++messagesSentAmount
-        ChatUtils.debug("Found message: $messagesSentAmount")
         if (messagesSentAmount != 2) return
-        ChatUtils.debug("Starting timer")
+        ChatUtils.debug("Starting Relic timer")
         timerEnd = SimpleTimeMark.now() + 4.seconds
+        relic = Relic.getRelic()
     }
 
     @SubscribeEvent
     fun onRenderWorld(event: LorenzRenderWorldEvent) {
         if (!isEnabled()) return
+        val relic = relic ?: return
         if (timerEnd.isInPast()) return
-        //if (LocationUtils.playerLocation().y > 20) return
-        locations.forEach { location ->
-            val text = "§d" + timerEnd.timeUntil().format(showMilliSeconds = true)
-            event.drawString(location.up(2.0), text, true)
+        val text = relic.color.getChatColor() + timerEnd.timeUntil().format(showMilliSeconds = true)
+        event.drawString(relic.location.up(2.0), text, true)
+    }
+
+    private enum class Relic(val location: LorenzVec, val color: LorenzColor, private val dungeonClass: DungeonAPI.DungeonClass) {
+        PURPLE(
+            LorenzVec(56, 8, 132),
+            LorenzColor.DARK_PURPLE,
+            DungeonAPI.DungeonClass.HEALER
+        ),
+        GREEN(
+            LorenzVec(20, 6, 94),
+            LorenzColor.GREEN,
+            DungeonAPI.DungeonClass.TANK
+        ),
+        RED(
+            LorenzVec(20, 6, 59),
+            LorenzColor.RED,
+            DungeonAPI.DungeonClass.BERSERK
+        ),
+        BLUE(
+            LorenzVec(91, 6, 94),
+            LorenzColor.BLUE,
+            DungeonAPI.DungeonClass.MAGE
+        ),
+        ORANGE(
+            LorenzVec(92, 6, 56),
+            LorenzColor.GOLD,
+            DungeonAPI.DungeonClass.ARCHER
+        ),
+        ;
+
+        companion object {
+            fun getRelic() = entries.find { it.dungeonClass == DungeonAPI.playerClass }
         }
-        /*val text = "§d" + timerEnd.timeUntil().format(showMilliSeconds = true)
-        event.drawString(location.up(2.0), text, true)*/
     }
 
     private fun isEnabled() =
