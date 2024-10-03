d27a797f0b0b15291c5427c86313bd104100889b
Empa <42304516+ItsEmpa@users.noreply.github.com>
2024-10-03 21:53:14 +0200

    Fix: Auto Updater (#26)
    Co-authored-by: ItsEmpa <itsempa@users.noreply.github.com>

--- src/main/kotlin/com/ratons/config/features/About.java
+++ src/main/kotlin/com/ratons/config/features/About.java
@@ -1,8 +1,10 @@
 package com.ratons.config.features;
 
 import at.hannibal2.skyhanni.deps.moulconfig.annotations.ConfigEditorBoolean;
+import at.hannibal2.skyhanni.deps.moulconfig.annotations.ConfigEditorDropdown;
 import at.hannibal2.skyhanni.deps.moulconfig.annotations.ConfigOption;
 import com.google.gson.annotations.Expose;
+import com.ratons.features.misc.update.UpdateStream;
 
 public class About {
 
@@ -10,6 +12,11 @@ public class About {
     @ConfigOption(name = "Auto Updates", desc = "Automatically updates when a new version is found.")
     @ConfigEditorBoolean
     public boolean autoUpdates = true;
+
+    @Expose
+    @ConfigOption(name = "Update Stream", desc = "Select the update stream.")
+    @ConfigEditorDropdown
+    public UpdateStream updateStream = UpdateStream.FULL;
 
     @Expose
     @ConfigOption(name = "Debug", desc = "Developer setting for checking debug messages.")
