a5cb95ab6c3101b924548022b8af212f05482630
Empa <itsempa@users.noreply.github.com>
2024-11-05 18:48:51 +0100

    remove unused import

--- src/main/kotlin/com/ratons/data/PacketData.kt
+++ src/main/kotlin/com/ratons/data/PacketData.kt
@@ -3,7 +3,6 @@ package com.ratons.data
 import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.events.minecraft.packet.PacketReceivedEvent
 import com.ratons.events.ServerTickEvent
-import com.ratons.utils.ChatUtils
 import net.minecraft.network.play.server.S32PacketConfirmTransaction
 
 object PacketData {
