f5e51ad26c4638a2047ccb112222cf8e9d6ea2ac
Empa <42304516+ItsEmpa@users.noreply.github.com>
2024-12-18 00:03:54 +0100

    Fix Skyhanni version crash (#36)
    Co-authored-by: Empa <itsempa@users.noreply.github.com>

--- src/main/kotlin/com/ratons/features/instances/PartyFinderFeatures.kt
+++ src/main/kotlin/com/ratons/features/instances/PartyFinderFeatures.kt
@@ -1,11 +1,12 @@
 package com.ratons.features.instances
 
+import at.hannibal2.skyhanni.api.event.HandleEvent
 import at.hannibal2.skyhanni.events.InventoryCloseEvent
 import at.hannibal2.skyhanni.events.InventoryOpenEvent
 import at.hannibal2.skyhanni.events.RenderInventoryItemTipEvent
 import at.hannibal2.skyhanni.utils.InventoryUtils
 import at.hannibal2.skyhanni.utils.ItemUtils.getLore
-import at.hannibal2.skyhanni.utils.RegexUtils.matches
+import at.hannibal2.skyhanni.utils.RegexUtils.find
 import com.ratons.Ratons
 import com.ratons.modules.RatModule
 import net.minecraft.item.ItemStack
@@ -16,8 +17,8 @@ object PartyFinderFeatures {
 
     private val config get() = Ratons.feature.instancesConfig.partyFinder
 
-    private val partyPattern = ".*('s Party)".toPattern()
-    private val dungeonPartyPattern = "§7Dungeon: .+".toPattern()
+    private val partyPattern = "'s Party$".toPattern()
+    private val dungeonPartyPattern = "^§7Dungeon: ".toPattern()
 
     private val stackSizes = mutableMapOf<Int, Int>()
 
@@ -26,12 +27,12 @@ object PartyFinderFeatures {
         partySize(event.inventoryItems)
     }
 
-    @SubscribeEvent
+    @HandleEvent(onlyOnSkyblock = true)
     fun onRenderItemTip(event: RenderInventoryItemTipEvent) {
         val slot = event.slot
         if (!config.partySizeDisplay || slot.slotNumber != slot.slotIndex) return
 
-        if (stackSizes.contains(slot.slotIndex)) event.stackTip = stackSizes[slot.slotIndex].toString()
+        event.stackTip = stackSizes[slot.slotIndex]?.toString() ?: return
     }
 
     @SubscribeEvent
@@ -42,12 +43,11 @@ object PartyFinderFeatures {
     private fun partySize(inventory: Map<Int, ItemStack>) {
         if (InventoryUtils.openInventoryName() != "Party Finder" || !config.partySizeDisplay) return
         for ((slot, item) in inventory){
-            if (partyPattern.matches(item.displayName)) {
+            if (partyPattern.find(item.displayName)) {
 
-                val isDungeon = dungeonPartyPattern.matches(item.getLore()[0])
-                var openSlots = 0
+                val isDungeon = dungeonPartyPattern.find(item.getLore()[0])
 
-                item.getLore().forEach { if (it == "§8 Empty") ++openSlots }
+                val openSlots = item.getLore().count { it == "§8 Empty" }
 
                 stackSizes[slot] = (if (isDungeon) 5 else 4) - openSlots
             }
